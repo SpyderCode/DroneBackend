3071b8a415e72596300c3e881db748d5a62fe346
SpyderCode <Spinneowo@gmail.com>
2024-10-22 00:09:39 -0600

    yeet

--- src/db.js
+++ src/db.js
@@ -1,8 +1,10 @@
 import mongoose from 'mongoose';
-
+import 'dotenv/config';
 export const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/drones');
+        //await mongoose.connect('mongodb://localhost:27017/drones');
+        const url = process.env.DB_CONNECTION_STRING;
+        await mongoose.connect(url+'/drones');
         console.log("Base de datos conectados")
     } catch (error) {
         console.error(error);
